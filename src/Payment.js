be191bdb0adf4cbfafd81532da41109e250b305b
vijayAnand <vijayanand9367@gmail.com>
2024-11-20 19:57:26 +0000

    Git test

--- src/Payment.js
+++ src/Payment.js
@@ -4,7 +4,7 @@ import React from 'react';
 const Payment = () => {
   return (
     <div>
-      <h2>Payments</h2>
+      <h2>Payment</h2>
       <form>
         {/* Include your payment fields and logic here */}
         <input type="text" placeholder="Member ID" />
